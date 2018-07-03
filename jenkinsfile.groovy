01a6af752ab74530f67e47e053f360ad1c5f7d4a
Zajs <aga.zajs@gmail.com>
2018-07-03 15:28:02 +0300

    New Release Text With Multi line

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -22,7 +22,7 @@ def isReleaseJob() {
 def generateTagComment(releaseVersion) {
     commitsList = sh(
             returnStdout: true,
-            script: "git log `git tag --sort=-taggerdate | head -1`..HEAD --pretty=\"@%an %B\""
+            script: "git log `git tag --sort=-taggerdate | head -1`..HEAD --pretty=\"@%an %h %B\""
     ).trim()
     return "${commitsList}"
 }
