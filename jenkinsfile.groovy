1e0a6819cf831d576772af35f5be13516cd79d27
Zajs <aga.zajs@gmail.com>
2018-07-29 21:51:18 +0300

    - shared library for pipelines

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,18 +1,15 @@
+def repository = 'hydro-serving-sidecar'
 
-node("JenkinsOnDemand") {
-    def repository = 'hydro-serving-sidecar'
-
-    def buildFunction = {
-        def curVersion=getVersion()
-        sh "docker build -t hydrosphere/serving-sidecar:${curVersion} ."
-    }
-
-    pipelineCommon(
-            repository,
-            false, //needSonarQualityGate,
-            ["hydrosphere/serving-sidecar"],
-            {},//collectTestResults, do nothing
-            buildFunction,
-            buildFunction
-    )
+def buildFunction = {
+    def curVersion = getVersion()
+    sh "docker build -t hydrosphere/serving-sidecar:${curVersion} ."
 }
+
+pipelineCommon(
+        repository,
+        false, //needSonarQualityGate,
+        ["hydrosphere/serving-sidecar"],
+        {},//collectTestResults, do nothing
+        buildFunction,
+        buildFunction
+)
